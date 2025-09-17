000bf758953e3d5211c7065d27b55e3b3160ca02
Lee King Hung <slkhung@hotmail.com>
2025-09-17 21:23:31 +0800

    Add default theme

--- app/src/main/java/com/example/dogbreedschallenge/ui/AppActivity.kt
+++ app/src/main/java/com/example/dogbreedschallenge/ui/AppActivity.kt
@@ -1,7 +1,39 @@
 package com.example.dogbreedschallenge.ui
 
+import android.graphics.Color
+import android.os.Bundle
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.Composable
+import com.example.dogbreedschallenge.ui.theme.AppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AppActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+
+		// Edge-To-Edge config
+		val navigationBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
+		enableEdgeToEdge(navigationBarStyle = navigationBarStyle)
+
+		super.onCreate(savedInstanceState)
+
+		setContent {
+
+			AppTheme {
+				RootContent()
+			}
+
+		}
+
+	}
+
+}
+
+@Composable
+private fun RootContent() {
 
 }
