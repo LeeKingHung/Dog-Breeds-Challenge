1c9d0dc19dbfcd9b12d4d53e718a11f8282b54a0
Lee King Hung <slkhung@hotmail.com>
2025-09-18 16:34:52 +0800

    Update ViewModel with Repository

--- app/src/main/java/com/example/dogbreedschallenge/ui/AppViewModel.kt
+++ app/src/main/java/com/example/dogbreedschallenge/ui/AppViewModel.kt
@@ -1,21 +1,48 @@
 package com.example.dogbreedschallenge.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.dogbreedschallenge.data.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class AppViewModel  : ViewModel() {
-	
-	private val _uiStateFlow = MutableStateFlow(UiState())
+@HiltViewModel
+class AppViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
+
+	private val _uiStateFlow = MutableStateFlow(UiState(inputsState = LoadingState.None))
 	val uiStateFlow = _uiStateFlow.asStateFlow()
+
+	init {
+		loadAllBreeds()
+	}
 
 	fun updateAnswer(answer: String) {
 		_uiStateFlow.update { it.copy(answer = answer) }
 	}
 
 	fun checkAnswer() {
-		
+
 	}
-	
+
+	 fun loadAllBreeds() {
+
+		_uiStateFlow.update { it.copy(inputsState = LoadingState.Loading) }
+
+		viewModelScope.launch(context = Dispatchers.IO) {
+
+			repository.getAllDogs().onSuccess { breeds ->
+				_uiStateFlow.update { it.copy(inputsState = LoadingState.Success(breeds)) }
+			}.onFailure {
+				_uiStateFlow.update { it.copy(inputsState = LoadingState.Error) }
+			}
+
+		}
+
+	}
+
 }
--- app/src/main/java/com/example/dogbreedschallenge/ui/UiState.kt
+++ app/src/main/java/com/example/dogbreedschallenge/ui/UiState.kt
@@ -1,7 +1,14 @@
 package com.example.dogbreedschallenge.ui
 
 data class UiState(
-	val inputs: List<String> = listOf("Affenpinscher", "African", "Airedale"),
+	val inputsState: LoadingState<List<String>>,
 	val answer: String = "",
 	val isAnswerCorrect: Boolean? = null
 )
+
+sealed class LoadingState<out T> {
+	object None : LoadingState<Nothing>()
+	object Loading : LoadingState<Nothing>()
+	data class Success<T>(val data: T) : LoadingState<T>()
+	object Error : LoadingState<Nothing>()
+}
